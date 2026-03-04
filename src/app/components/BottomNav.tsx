e965be386b9dbf7024a91391d75764e646646b0d
baburaj <baburajnk19@gmail.com>
2026-03-04 17:02:24 +0530

    2nd commit

--- src/app/components/BottomNav.tsx
+++ src/app/components/BottomNav.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router-dom";
 import { Home, ShoppingBag, Wallet, Grid3x3, User } from "lucide-react";
 
 interface BottomNavProps {
